09f697627157c749eca5e4ec5ffad4334cf15f61
Jose <Jose@Deadwind>
2013-03-29 23:50:00 +0000

    GettersToOperators

--- src/SyntaxTree/operators/BinaryOperator.java
+++ src/SyntaxTree/operators/BinaryOperator.java
@@ -9,32 +9,26 @@ public class BinaryOperator extends Operator {
     private static final BinaryOperator AND = new BinaryOperator("&&", "and");
     private static final BinaryOperator OR = new BinaryOperator("||", "or");
 
-   
     public static BinaryOperator getADD() {
         return ADD;
     }
 
-    
     public static BinaryOperator getSUBSTRACT() {
         return SUBSTRACT;
     }
 
-  
     public static BinaryOperator getMULTIPLY() {
         return MULTIPLY;
     }
 
-   
     public static BinaryOperator getDIVISION() {
         return DIVISION;
     }
 
-   
     public static BinaryOperator getAND() {
         return AND;
     }
 
-    
     public static BinaryOperator getOR() {
         return OR;
     }
--- src/SyntaxTree/operators/UnaryOperator.java
+++ src/SyntaxTree/operators/UnaryOperator.java
@@ -7,17 +7,14 @@ public class UnaryOperator extends Operator {
     private static final UnaryOperator COS = new UnaryOperator("cos", "cos");
     private static final UnaryOperator TAN = new UnaryOperator("tan", "tan");
 
-   
     public static UnaryOperator getNOT() {
         return NOT;
     }
 
-    
     public static UnaryOperator getSIN() {
         return SIN;
     }
 
-   
     public static UnaryOperator getCOS() {
         return COS;
     }
